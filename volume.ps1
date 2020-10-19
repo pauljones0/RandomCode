e93fa91004af15bd1e589ba5bf7d94d64be49ac5
AJuneSlop <32859666+AJuneSlop@users.noreply.github.com>
2020-10-18 19:31:14 -0600

    Update volume.ps1
    Edited it so no extra program is needed.

--- volume.ps1
+++ volume.ps1
@@ -1,8 +1,19 @@
 	while(1){
-$time = Get-Random -Minimum 45 -Maximum 60
+$time = Get-Random -Minimum 1 -Maximum 2
 start-sleep -seconds $time
+
+
+
+#$obj = new-object -com wscript.shell 
+#$obj.SendKeys([char]173) #this requires no extra programs, but the mute button apppears.
 cmd /c powershell.exe "nircmd.exe mutesysvolume 0"
-$time = Get-Random -Minimum 5 -Maximum 15
+
+
+$time = Get-Random -Minimum 1 -Maximum 2
 start-sleep -seconds $time
-cmd /c powershell.exe "nircmd.exe mutesysvolume 1"
+
+
+#$obj = new-object -com wscript.shell 
+#$obj.SendKeys([char]173) #ditto
+cmd /c powershell.exe "nircmd.exe mutesysvolume 1" #this is nice because nothing new appears on the screen.
 }
