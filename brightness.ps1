b618ab1bb9c768fe3a767b2543cc631235b39fed
AJuneSlop <32859666+AJuneSlop@users.noreply.github.com>
2020-10-31 18:37:47 -0600

    Update brightness.ps1
    Optimized code

--- brightness.ps1
+++ brightness.ps1
@@ -1,18 +1,17 @@
-$low = 80
-$high = 100
-$sleepseconds = 5
+  
+$low = 0
+$high = 30
+$sleep = 200
 
 while($true){ #forever loop
 for ($i = $high; $i -ge $low; $i--)
 {
 (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,$i) #makes the screen brighter
-Start-Sleep -m 200 #takes 200 millisecond breaks
+Start-Sleep -m $sleep
 }
-Start-Sleep -s $sleepseconds
 for ($i = $low; $i -le $high; $i++)
 {
 (Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,$i) #dims the screen
-Start-Sleep -m 200
+Start-Sleep -m $sleep
 }
-Start-Sleep -s $sleepseconds
 }
