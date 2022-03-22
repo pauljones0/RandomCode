1200ea599efc8b4cbfafe05eca820301df1ec2a1
pauljones0 <32859666+pauljones0@users.noreply.github.com>
2022-03-21 22:35:54 -0600

    Update RGB_cycler.py
    Added Grovee into the mix. Grovee could be updated more frequently (up to 100 times per minute), but this seems to work for now.

--- RGB_cycler.py
+++ RGB_cycler.py
@@ -1,15 +1,46 @@
 import sys
 from phue import Bridge
 from lifxlan import LifxLAN
+from requests import put
+from math import fabs, fmod,floor
 
 def main():
     
     #setup phillips hue and initialize the bridge
-    b = Bridge(ip='YOUR BRIDGE IP HERE',username='YOUR USERNAME HERE')
+    b = Bridge(ip=<YOUR IP ADDRESS>,username=<YOUR USERNAME>)
     lights = b.lights
     
     #setup Lifx bulbs
-    lifx = LifxLAN(9)
+    lifx = LifxLAN(<NUMBER OF LIGHTS CAN BE DISCOVERED>)
+    lifx.set_power_all_lights("on")
+        
+    def calc(hue):
+        '''
+        This converts the normal 16bit HSV values to RGB ones and returns them.
+        '''
+        X= 1-fabs(fmod(hue/10922.6667,2)-1)
+        X= 255 if X==1.0 else floor(X*256)
+        Y=floor(fmod(hue/10922.6667,6))
+        rgbval=[(255,X,0),(X,255,0),(0,255,X),(0,X,255),(X,0,255),(255,0,X)]
+        return rgbval[Y]
+    
+    def hue_grovee(new_hue):
+        '''
+        Makes the calls to update the RGB values
+        '''
+        rgb=calc(new_hue)
+        red=rgb[0]
+        green=rgb[1]
+        blue=rgb[2]
+        requests.put(url='https://developer-api.govee.com/v1/devices/control', headers={'Govee-API-KEY':<API KEY HERE>},json={'cmd':{'name': 'color', 'value': {"r":red,"g":green,"b":blue}},'device': <MAC ADDRESS HERE>, 'model': <MODEL HERE>})
+    
+    #Find and setup Grovee
+    # response = requests.get('https://developer-api.govee.com/v1/devices', headers={'Govee-API-KEY':'19d6dbe9-1265-4300-8420-1a96a3e53077'})
+    # jresponse = response.json()
+    # print(jresponse)
+    #set the brightness and values to the max
+    requests.put(url='https://developer-api.govee.com/v1/devices/control', headers={'Govee-API-KEY':<API KEY HERE>},json={'cmd':{'name': 'turn', 'value': 'on'},'device': <MAC ADDRESS HERE>, 'model': <MODEL HERE>})
+    requests.put(url='https://developer-api.govee.com/v1/devices/control', headers={'Govee-API-KEY':<API KEY HERE>},json={'cmd':{'name': 'brightness', 'value': 100},'device': <MAC ADDRESS HERE>, 'model': <MODEL HERE>})
     
     #how long each cycle should last
     totalTime = 15 # in seconds
@@ -23,7 +54,7 @@ def main():
         light.transitiontime = transitionTime * 10
         light.brightness = 254
         light.saturation = 254
-        # light.on = True # uncomment to turn all lights on
+        light.on = True # uncomment to turn all lights on
     
     #loop through
     hue = 0
@@ -31,6 +62,7 @@ def main():
         for light in lights:
             light.hue = hue
         lifx.set_color_all_lights(color=[hue,65535,65535,3500], duration=transitionTime*1000, rapid=True)
+        hue_grovee(hue)
         hue = (hue + hueIncrement) % maxHue
         sleep(transitionTime)
                    
