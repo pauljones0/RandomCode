21a534a40d6ef7a9c53471cefabf9ab24f1717e6
pauljones0 <32859666+pauljones0@users.noreply.github.com>
2022-04-18 14:54:59 -0600

    Update LeetCodeConnect.py
    Firefox headless works, while Chrome headless is a pain. Takes 4 more seconds, but it's headless which is easier for me.

--- LeetCodeConnect.py
+++ LeetCodeConnect.py
@@ -13,25 +13,30 @@ data = request_reply.json()
 
 new_string = f'I\'ve Solved {data["num_solved"]} LeetCode problems, {data["ac_easy"]} Easy, {data["ac_medium"]} Medium and {data["ac_hard"]} Hard. This string is updated programmatically using Python and Selenium.'
 
-print(combined_string)
-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-options = webdriver.ChromeOptions()
+options = webdriver.FirefoxOptions()
 options.add_argument("--headless")
-driver= webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+driver= webdriver.Firefox(service=Service(GeckoDriverManager().install()),options=options)
 driver.get("https://www.linkedin.com")
 with open('www.linkedin.com.cookies.json') as f:
     linkedincookies = json.load(f)
 for i in linkedincookies:
     driver.add_cookie(i)
+    
+    
 driver.get(EDIT_ABOUT)
 
 textarea = WebDriverWait(driver,5).until(lambda d: d.find_element(By.XPATH, EDIT_XPATH))
+
+action = webdriver.ActionChains(driver)
+action.send_keys_to_element(textarea, Keys.ARROW_LEFT, Keys.ARROW_LEFT).key_down(Keys.SHIFT).send_keys(Keys.HOME,Keys.ARROW_UP,Keys.ARROW_RIGHT,Keys.ARROW_RIGHT).key_up(Keys.SHIFT).send_keys(new_string).perform()
+driver.find_element(By.XPATH, '//*[@id="ember90"]').click()
+driver.quit()
 
 action = webdriver.ActionChains(driver)
 action.send_keys_to_element(textarea, Keys.ARROW_LEFT, Keys.ARROW_LEFT).key_down(Keys.SHIFT).send_keys(Keys.HOME,Keys.ARROW_UP,Keys.ARROW_RIGHT,Keys.ARROW_RIGHT).key_up(Keys.SHIFT).send_keys(new_string).perform()
