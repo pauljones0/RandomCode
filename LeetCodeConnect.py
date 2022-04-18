f0b66fd9cc87f52e6479be4528aa8310e3b36905
pauljones0 <32859666+pauljones0@users.noreply.github.com>
2022-04-18 13:45:29 -0600

    Update LeetCodeConnect.py
    After removing emojis from the inputted string and more careful insertion, I've saved 40% of the time. Now down to 15 seconds for the browser, 1 second for the LEETCODE call. Now trying to do it headless.

--- LeetCodeConnect.py
+++ LeetCodeConnect.py
@@ -1,45 +1,37 @@
 import requests
 import json
 
-#Use the "Export cookie JSON file for Puppeteer" Chrome extension to generate these files.
-#A JSON file will be generated containing all the cookies for the website you are currently on.
 with open('leetcode.com.cookies.json') as f:
 	leetcookies = json.load(f)
 formatted_cookies={}
 
 for i in leetcookies:
     formatted_cookies[i['name']]=i['value']
-#print(formatted_cookies)
-#This is the LeetCode API, if you are currently logged in.
 Url = "https://leetcode.com/api/problems/algorithms/"
 request_reply = requests.get( Url, cookies=formatted_cookies)
 data = request_reply.json()
 
-#The default string is whatever blurb is currently in your LinkedIn about section that you want to stay there
-default_string = "I am interested in applying statistical inferences to large datasets, currently working on AWS certificates.😃\n\n"
-
-#This blurb is generated using the LeetCode JSON
-leetcode_string = f'🔥🔥I\'ve Solved {data["num_solved"]} LeetCode problems, {data["ac_easy"]} Easy, {data["ac_medium"]} Medium and {data["ac_hard"]} Hard. This string is updated programmatically using Python and Selenium.🔥🔥'
-combined_string = default_string+leetcode_string
+new_string = f'I\'ve Solved {data["num_solved"]} LeetCode problems, {data["ac_easy"]} Easy, {data["ac_medium"]} Medium and {data["ac_hard"]} Hard. This string is updated programmatically using Python and Selenium.'
 
 print(combined_string)
 
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-driver= webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+driver= webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.linkedin.com")
 with open('www.linkedin.com.cookies.json') as f:
     linkedincookies = json.load(f)
 for i in linkedincookies:
     driver.add_cookie(i)
-driver.get("https://www.linkedin.com/in/paul-jones-969577180/edit/forms/summary/new/?profileFormEntryPoint=PROFILE_SECTION&trackingId=l85%2BWyFQRMW17ic3JgyfWQ%3D%3D")
+driver.get(EDIT_ABOUT)
 
-textarea = WebDriverWait(driver,5).until(lambda d: d.find_element(By.XPATH, '//*[@id="multiline-text-form-component-profileEditFormElement-SUMMARY-profile-ACoAACrMZ6EBNDja1YhNynYMasB0eX8VQ4LWTzc-summary"]'))
-textarea.send_keys(Keys.CONTROL, 'a')
-textarea.send_keys(combined_string)
+textarea = WebDriverWait(driver,5).until(lambda d: d.find_element(By.XPATH, EDIT_XPATH))
+
+action = webdriver.ActionChains(driver)
+action.send_keys_to_element(textarea, Keys.ARROW_LEFT, Keys.ARROW_LEFT).key_down(Keys.SHIFT).send_keys(Keys.HOME,Keys.ARROW_UP,Keys.ARROW_RIGHT,Keys.ARROW_RIGHT).key_up(Keys.SHIFT).send_keys(new_string).perform()
 driver.find_element(By.XPATH, '//*[@id="ember90"]').click()
 driver.quit()
